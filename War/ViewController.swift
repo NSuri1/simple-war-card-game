f660f0bd3606e96d2fad538a58b275aad5a358ed
Macbook <nalinsuri@gmail.com>
2016-10-14 22:54:41 -0400

    remove switch case, add array

--- War/ViewController.swift
+++ War/ViewController.swift
@@ -25,47 +25,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     @IBAction func deal(sender: UIButton){
-        print("Deal Button Pressed")
+        let cards: [UIImage] = [#imageLiteral(resourceName: "card2"), #imageLiteral(resourceName: "card3"), #imageLiteral(resourceName: "card4"), #imageLiteral(resourceName: "card5"), #imageLiteral(resourceName: "card6"), #imageLiteral(resourceName: "card7"), #imageLiteral(resourceName: "card8"), #imageLiteral(resourceName: "card9"), #imageLiteral(resourceName: "card10"), #imageLiteral(resourceName: "jack"), #imageLiteral(resourceName: "queen"), #imageLiteral(resourceName: "king"), #imageLiteral(resourceName: "ace")]
         
+        var leftRandom = Int(arc4random_uniform(13))
+        LeftCardView.image = cards[leftRandom]
         
-        LeftCardView.image = randomCards()
-        
-        
-        RightCardView.image = randomCards()
+        var rightRandom = Int(arc4random_uniform(13))
+        RightCardView.image = cards[rightRandom]
     }
     
-    func randomCards() ->UIImage {
-        switch arc4random_uniform(13){
-        case 0:
-            return UIImage(named: "card2")!
-        case 1:
-            return UIImage(named: "card3")!
-        case 2:
-            return UIImage(named: "card4")!
-        case 3:
-            return UIImage(named: "card5")!
-        case 4:
-            return UIImage(named: "card6")!
-        case 5:
-            return UIImage(named: "card7")!
-        case 6:
-            return UIImage(named: "card8")!
-        case 7:
-            return UIImage(named: "card9")!
-        case 8:
-            return UIImage(named: "card10")!
-        case 9:
-            return UIImage(named: "jack")!
-        case 10:
-            return UIImage(named: "queen")!
-        case 11:
-            return UIImage(named: "king")!
-        case 12:
-            return UIImage(named: "ace")!
-        default:
-            return UIImage(named: "back")!
-        }
-
-    }
-
 }
